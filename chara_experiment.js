b763781b50d91ccd9b76bb6dc6ea7cbcd9619fdb
min0025 <122358935+min0025@users.noreply.github.com>
2025-07-29 14:58:37 +0900

    Update chara_experiment.js

--- chara_experiment.js
+++ chara_experiment.js
@@ -39,14 +39,17 @@ const intro = {
     type: jsPsychHtmlButtonResponse,
     stimulus: `
       <h1>主観評価実験</h1>
-      <p>この調査では、アニメの様々な1人称のテキストを見て各設問に評価してもらいます</p>
+      <p>この調査では、様々な1人称および2人称の単語を見て各設問に回答してもらいます</p>
       <p>前半と後半の2段階のセクションに分かれています</p>
-      <p>前半セクションでは、各質問に1から7の7段階評価をしてください</p>
+      <p>前半セクションでは、単語に対する設問に対して自分が感じる度合いを1から7の7段階で評価してください</p>
     `,
     choices: ['開始する']
 };
 
-const stimuli = ["ぼく", "わたし"];
+const stimuli = ["おれ", "ぼく", "おいら", "わし", "あんた", "きさま", "あたい", "あたし", "わちき", "わらわ", "おぬし", "なんじ", "わがはい", "しょくん", "わたくし"];
+
+// 刺激をランダムに並べ替える
+const shuffled_stimuli = jsPsych.randomization.shuffle(stimuli);
 
 const adjectives = [
     { left: "男性的", right: "女性的" },
@@ -62,7 +65,7 @@ const likert_scale = ["1", "2", "3", "4", "5", "6", "7"];
 const likert_bio = ["成人男性", "少年", "おじいさん", "上司", "成人女性", "少女", "おばあさん", "お嬢様", "奥様", "幼児"]
 
 // SD法での調査
-const trials_7 = stimuli.map(stim => {
+const trials_7 = shuffled_stimuli.map(stim => {
     const questions = adjectives.map((pair) => ({
         prompt: `<b style="font-size: 24px">${pair.left} - ${pair.right}</b><br>`,
         labels: likert_scale,
@@ -83,17 +86,20 @@ const trials_7 = stimuli.map(stim => {
 });
 
 // キャラクタの人物像想起
-const trials_bio = stimuli.map(stim => ({
-    type: jsPsychSurveyLikert,
-    scale_width: 1400,
+const trials_bio = shuffled_stimuli.map(stim => ({
+    type: jsPsychSurveyMultiChoice,
     preamble: `<div class="stimulus-box">
                  <p style="font-size: 50px;">${stim}</p>
                </div>`,
-    questions: [{
+    questions: [
+      {
         prompt: `<b style="font-size: 24px">表示された単語から連想する人物像を選んでください</b><br>`,
-        labels: likert_bio,
-        required: true
-    }],
+        name: 'Characteristics', 
+        options: likert_bio, 
+        required: true,
+        horizontal: true
+      }
+    ],
     data: { stim: stim, block: 'BIO' },
     post_trial_gap: 1000
 }));
@@ -103,15 +109,15 @@ const intermission = {
     type: jsPsychHtmlButtonResponse,
     stimulus: `
       <h2>つづいて、後半のセクションです</h2>
-      <p>後半セクションでは、各質問から連想する人物像を1つ選んで回答してください</p>
-      <p>準備ができ次第、下のボタンから先に進んでください</p>`,
+      <p>後半セクションでは、単語から連想する人物像を1つ選んで回答してください</p>
+      <p>準備ができ次第、下のボタンを押して先に進んでください</p>`,
     choices: ["進む"]
 };
 
 // 性別と年齢入力欄
 const demographics = {
     type: jsPsychSurveyHtmlForm,
-    preamble: "<h2>アンケート</h2><p>最後にご自身の以下項目にご回答をお願いします。</p>",
+    preamble: "<h2>お疲れ様でした</h2><p>最後にご自身の以下項目にご回答をお願いします。</p>",
     html: `
       <p>
         性別:
